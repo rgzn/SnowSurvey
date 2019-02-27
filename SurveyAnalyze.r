42ecaa51e8f7b9425602d3df35aaab80e4b2f0d8
Weissman <Jonathan.Weissman@wildlife.ca.gov>
2019-02-27 15:45:09 -0800

    comments

--- SurveyAnalyze.r
+++ SurveyAnalyze.r
@@ -10,8 +10,6 @@ snow = getAllCourseRecords(courses)
 # Merge the dataframes
 
 snow_full = snow %>% left_join(courses, by = "Course")
-snow_full %>% filter(is.na("<!--"))
-
 
 # correct non-negative longitude:
 courses = 
@@ -21,16 +19,18 @@ courses =
 courses = 
   courses %>% mutate_at("April_1_Avg_inches", function(x) pmax(x,0))
 
+# create simple features dataframe for mapping
 courses_sf = st_as_sf(x = courses,
                       coords = c("Longitude","Latitude"),
                       crs = "+proj=longlat +datum=WGS84")
 
+
+# Interactive map of courses:
 mapviewOptions(basemaps = c("Esri.WorldShadedRelief", "OpenStreetMap", "Esri.WorldImagery", "OpenTopoMap"),
                raster.palette = grey.colors,
                vector.palette = colorRampPalette(c(rgb(1,1,1,0.5),rgb(0,0,1,0.5)), alpha=TRUE)(12),
                na.color = "magenta",
                layers.control.pos = "topright")
-
 mapview(courses_sf,
         zcol = "April_1_Avg_inches",
         lwd = 0)
